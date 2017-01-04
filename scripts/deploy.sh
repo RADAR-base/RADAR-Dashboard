65b229e0cfc01ef272e2f7c4276333ae6adcff46
Herculano Campos <im@herkulano.com>
2017-01-05 00:28:25 +0200

    chore(deploy): remove 'ssh'

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rsync -rvz --delete-after --exclude=questionnaire \
-  'ssh' $TRAVIS_BUILD_DIR/dist/ $DEPLOY_USER@$DEPLOY_SERVER:/www/dashboard
+  $TRAVIS_BUILD_DIR/dist/ $DEPLOY_USER@$DEPLOY_SERVER:/www/dashboard
 
 exit 0
