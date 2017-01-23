9b6c947a433fa04404b059cd53314fa0cba357b3
herkulano <im@herkulano.com>
2017-01-23 19:57:28 +0200

    chore(protractor): add browserstack

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,31 +2,85 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 /*global jasmine */
-var SpecReporter = require('jasmine-spec-reporter');
+var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+var browserstack = require('browserstack-local');
 
 exports.config = {
-  allScriptsTimeout: 11000,
+  seleniumAddress: 'http://hub-cloud.browserstack.com/wd/hub',
+  allScriptsTimeout: 120000,
+  getPageTimeout: 120000,
   specs: [
     './e2e/**/*.e2e-spec.ts'
   ],
-  capabilities: {
-    'browserName': 'chrome'
+  commonCapabilities: {
+    'name': 'radar-dashboard',
+    'build': 'Protractor Local',
+    'browserstack.user': process.env.BROWSERSTACK_USERNAME,
+    'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY,
+    'browserstack.local': true,
+    'browserstack.debug': true,
+    'resolution': '1024x768'
   },
-  directConnect: true,
+  multiCapabilities: [
+    {
+      'browserName': 'Chrome'
+    },
+    // {
+    //   'browserName': 'Firefox'
+    // },
+    // {
+    //   'browserName': 'Safari'
+    // },
+    // {
+    //   'browserName': 'Edge'
+    // }
+  ],
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () {
+    }
   },
   useAllAngular2AppRoots: true,
-  beforeLaunch: function() {
+  beforeLaunch: function () {
     require('ts-node').register({
       project: 'e2e'
     });
+
+    console.log('Connecting local');
+    return new Promise(function (resolve, reject) {
+      exports.bs_local = new browserstack.Local();
+      exports.bs_local.start({
+        'key': exports.config.commonCapabilities['browserstack.key']
+      }, function (error) {
+        if (error) return reject(error);
+        console.log('Connected. Now testing...');
+        resolve();
+      });
+    });
   },
-  onPrepare: function() {
+  onPrepare: function () {
     jasmine.getEnv().addReporter(new SpecReporter());
+  },
+  afterLaunch: function () {
+    return new Promise(function (resolve) {
+      exports.bs_local.stop(resolve);
+    });
   }
 };
+
+if (process.env['TRAVIS']) {
+  exports.config.browserStack.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+  exports.config.browserStack.build = 'Karma Travis #' +
+    process.env.TRAVIS_BUILD_NUMBER + ' [' +
+    process.env.TRAVIS_BUILD_ID + ']';
+}
+
+// Code to support common capabilities
+exports.config.multiCapabilities.forEach(function (caps) {
+  for (var i in exports.config.commonCapabilities) {
+    caps[i] = caps[i] || exports.config.commonCapabilities[i];
+  }
+});
