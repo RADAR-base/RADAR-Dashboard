0eeb01f38a6c850c2e2591898291aaf25d3af1fb
Herculano Campos <hi@herkulano.com>
2017-05-26 19:04:32 +0300

    fix style

--- src/app/components/source-list/source-list.component.spec.ts
+++ src/app/components/source-list/source-list.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { SourceListComponent } from './source-list.component';
+import { SourceListComponent } from './source-list.component'
 
 describe('SourceListComponent', () => {
-  let component: SourceListComponent;
-  let fixture: ComponentFixture<SourceListComponent>;
+  let component: SourceListComponent
+  let fixture: ComponentFixture<SourceListComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SourceListComponent ]
+      declarations: [SourceListComponent]
     })
-    .compileComponents();
-  }));
+      .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SourceListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(SourceListComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
