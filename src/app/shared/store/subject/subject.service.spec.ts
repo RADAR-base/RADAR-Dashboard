c2adc9247ef3900b9ad82575944230eef5fdba31
Herculano Campos <hi@herkulano.com>
2017-06-13 17:29:12 +0300

    fix test

--- src/app/shared/store/subject/subject.service.spec.ts
+++ src/app/shared/store/subject/subject.service.spec.ts
@@ -1,11 +1,17 @@
 import { inject, TestBed } from '@angular/core/testing'
+import { HttpModule } from '@angular/http'
 
 import { SubjectService } from './subject.service'
 
 describe('SubjectService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [SubjectService]
+      imports: [
+        HttpModule
+      ],
+      providers: [
+        SubjectService
+      ]
     })
   })
 
