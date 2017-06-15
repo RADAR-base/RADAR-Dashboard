5c66ca50b5d624166fb7a3fb9d7b03ae9ccf02f0
Herculano Campos <hi@herkulano.com>
2017-06-15 18:31:47 +0300

    add firebase endpoint

--- src/app/shared/testing/mocks/mock-params.ts
+++ src/app/shared/testing/mocks/mock-params.ts
@@ -1,4 +1,5 @@
 export const MockParams = {
   API_URI: 'http://radar-restapi.eu-west-1.elasticbeanstalk.com/api',
-  API_LOCAL: 'assets/data'
+  API_LOCAL: 'assets/data',
+  API_FIREBASE: 'https://radar-dashboard.firebaseio.com'
 }
