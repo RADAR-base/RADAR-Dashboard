a93bee744bb56b828a5ae4f6e7795f80fba52a92
Herculano Campos <hi@herkulano.com>
2017-06-16 14:00:26 +0300

    add firebase config
    add loading step to wait for config from firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,41 @@
 import { Component, OnInit } from '@angular/core'
 import { Store } from '@ngrx/store'
+import { Observable } from 'rxjs/Observable'
 
 import * as fromRoot from './shared/store/'
 import * as configAction from './shared/store/config/config.actions'
+import { AppConfig } from './shared/utils/config'
 
 @Component({
   selector: 'app-root',
   template: `
     <app-toolbar></app-toolbar>
     <div class="app-content">
-      <router-outlet></router-outlet>
+      <p *ngIf="isLoadingConfig$ | async; else pages">Loading...</p>
+      <ng-template #pages>
+        <router-outlet></router-outlet>
+      </ng-template>
     </div>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  isLoadingConfig$: Observable<boolean>
+
   constructor (
     private store: Store<fromRoot.State>
   ) {}
 
   ngOnInit () {
+    // Load config from firebase
     this.store.dispatch(new configAction.Load())
+
+    // Wait until load is complete
+    this.isLoadingConfig$ = this.store.select(fromRoot.getConfigLoading)
+
+    // Set config to global AppConfig
+    this.store.select(fromRoot.getConfigState)
+      .subscribe(config => AppConfig.config = config)
   }
 }
