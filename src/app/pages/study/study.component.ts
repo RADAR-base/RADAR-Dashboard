85136e061776c7eb1b41eef9608f2002249b3830
Herculano Campos <hi@herkulano.com>
2017-06-19 16:21:55 +0300

    add TakeUntilDestroy decorator

--- src/app/pages/study/study.component.ts
+++ src/app/pages/study/study.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnDestroy, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Router } from '@angular/router'
 import '@ngrx/core/add/operator/select'
 import { Store } from '@ngrx/store'
-import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/takeUntil'
 import { Observable } from 'rxjs/Observable'
-import { Subscription } from 'rxjs/Subscription'
 
 import * as fromRoot from '../../shared/store/index'
 import * as studyAction from '../../shared/store/study/study.actions'
 import { Study } from '../../shared/store/study/study.model'
 import * as subjectAction from '../../shared/store/subject/subject.actions'
 import { Subject } from '../../shared/store/subject/subject.model'
+import { TakeUntilDestroy } from '../../shared/utils/TakeUntilDestroy'
 
 @Component({
   selector: 'app-study-page',
@@ -18,33 +18,33 @@ import { Subject } from '../../shared/store/subject/subject.model'
     <p>Study Page</p>
 
     <p *ngIf="!isLoaded">Loading...</p>
+
+    <ng-container *ngIf="isStudyLoadedAndValid && isLoaded">
+      <p>{{ study$ | async | json}}</p>
+      <ul>
+        <li *ngFor="let subject of (subjects$ | async)">
+          <button (click)="navigateToSubject(subject.subjectId)">
+            ID: {{ subject.subjectId }} | Active: {{ subject.active }}
+          </button>
+        </li>
+      </ul>
+    </ng-container>
+
     <p *ngIf="!isStudyLoadedAndValid && isLoaded">
       Study with ID {{studyId}} not found.
     </p>
-
-    <p *ngIf="isStudyLoadedAndValid && isLoaded">{{ study$ | async | json}}</p>
-
-    <ul *ngIf="isStudyLoadedAndValid && isLoaded">
-      <li *ngFor="let subject of (subjects$ | async)">
-        <button (click)="navigateToSubject(subject.subjectId)">
-          ID: {{ subject.subjectId }} | Active: {{ subject.active }}
-        </button>
-      </li>
-    </ul>
   `,
   styleUrls: ['./study.component.scss']
 })
-export class StudyPageComponent implements OnInit, OnDestroy {
+@TakeUntilDestroy
+export class StudyPageComponent implements OnInit {
 
+  takeUntilDestroy // from TakeUntilDestroy
   studyId: string
   study$: Observable<Study>
   isStudyLoadedAndValid = false
-  isStudyLoadedAndValid$: Subscription
   isLoaded = false
-  isLoaded$: Subscription
   subjects$: Observable<Subject[]>
-
-  private route$: Subscription
 
   constructor (
     private router: Router,
@@ -54,16 +54,18 @@ export class StudyPageComponent implements OnInit, OnDestroy {
 
   ngOnInit () {
     // Get `studyId` from route and then get Study by `studyId`
-    this.route$ = this.route.params
+    this.route.params
+      .takeUntil(this.takeUntilDestroy())
       .subscribe(params => {
         this.studyId = params['studyId']
       })
 
     // If study is not loaded and not valid then fetch it
-    this.isStudyLoadedAndValid$ = this.store.select(fromRoot.getStudyIsLoadedAndValid)
+    this.store.select(fromRoot.getStudyIsLoadedAndValid)
+      .takeUntil(this.takeUntilDestroy())
       .subscribe(
         (isLoadedAndValid) => {
-          this.isStudyLoadedAndValid = isLoadedAndValid
+          this.isStudyLoadedAndValid = !!isLoadedAndValid
           if (isLoadedAndValid) {
             this.store.dispatch(new subjectAction.GetAll(this.studyId))
           } else {
@@ -73,8 +75,9 @@ export class StudyPageComponent implements OnInit, OnDestroy {
       )
 
     // Check if study is loaded
-    this.isLoaded$ = this.store.select(fromRoot.getStudyIsLoaded)
-      .subscribe(isLoaded => this.isLoaded = isLoaded)
+    this.store.select(fromRoot.getStudyIsLoaded)
+      .takeUntil(this.takeUntilDestroy())
+      .subscribe(isLoaded => this.isLoaded = !!isLoaded)
 
     this.study$ = this.store.select(fromRoot.getStudySelected)
     this.subjects$ = this.store.select(fromRoot.getSubjectAll)
@@ -82,12 +85,6 @@ export class StudyPageComponent implements OnInit, OnDestroy {
 
   navigateToSubject (id) {
     this.router.navigate(['subject', id], { relativeTo: this.route })
-  }
-
-  ngOnDestroy () {
-    this.route$.unsubscribe()
-    this.isStudyLoadedAndValid$.unsubscribe()
-    this.isLoaded$.unsubscribe()
   }
 
 }
