54666660122d216e72341c0b6b86e58d2d40b8e4
Herculano Campos <hi@herkulano.com>
2017-06-23 14:38:30 +0100

    add sensors after getting data from source service

--- src/app/shared/store/source/source.effects.ts
+++ src/app/shared/store/source/source.effects.ts
@@ -3,6 +3,7 @@ import { Actions, Effect, toPayload } from '@ngrx/effects'
 import { Action } from '@ngrx/store'
 import { Observable } from 'rxjs/Observable'
 
+import * as sensorsAction from '../sensors/sensors.actions'
 import * as sourceAction from './source.actions'
 import { Source } from './source.model'
 import { SourceService } from './source.service'
@@ -18,6 +19,12 @@ export class SourceEffects {
       return this.sourceService.getAll(payload)
         .map((data: Source[]) => new sourceAction.GetAllSuccess(data))
     })
+
+  @Effect()
+  getAllSuccess$: Observable<Action> = this.actions$
+    .ofType(sourceAction.GET_ALL_SUCCESS)
+    .map(toPayload)
+    .map(data => new sensorsAction.GetAll(data))
 
   constructor (
     private actions$: Actions,
