74f3bfbcf02fb2a479097ced5d50381a12959fc7
Herculano Campos <hi@herkulano.com>
2017-06-26 14:41:02 +0100

    add multi line graph

--- src/app/components/source-graphs/source-graphs.module.ts
+++ src/app/components/source-graphs/source-graphs.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core'
 
 import { ChartsModule } from '../charts/charts.module'
 import { GraphBaseComponent } from './graph-base/graph-base.component'
+import { GraphMultiLineComponent } from './graph-multi-line/graph-multi-line.component'
 import { GraphSingleLineComponent } from './graph-single-line/graph-single-line.component'
 import { SourceGraphsComponent } from './source-graphs.component'
 import { SourceGraphsService } from './source-graphs.service'
@@ -15,6 +16,7 @@ import { SourceGraphsService } from './source-graphs.service'
   declarations: [
     SourceGraphsComponent,
     GraphSingleLineComponent,
+    GraphMultiLineComponent,
     GraphBaseComponent
   ],
   providers: [
