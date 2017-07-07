1b7cc933043140efe9abd97dc25ad045680b24a1
Herculano Campos <hi@herkulano.com>
2017-07-07 13:49:45 +0100

    remove mock delay

--- src/app/shared/store/study/study.service.ts
+++ src/app/shared/store/study/study.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core'
 import { Http } from '@angular/http'
-import 'rxjs/add/observable/from'
-import 'rxjs/add/operator/delay'
 import { Observable } from 'rxjs/Observable'
 
 import { ErrorService } from '../../services/error.service'
@@ -15,7 +13,6 @@ export class StudyService {
   getAll (): Observable<Study[]> {
     // TODO: Change when API is ready
     return this.http.get(`${PARAMS.API_LOCAL}/mock-all-studies.json`)
-      .delay(1000)
       .map(res => res.json().dataset || [])
       .catch(ErrorService.handleError)
   }
@@ -23,7 +20,6 @@ export class StudyService {
   getById (id): Observable<Study> {
     // TODO: Change when API is ready
     return this.http.get(`${PARAMS.API_LOCAL}/mock-all-studies.json`)
-      .delay(1000)
       .map(res => res.json().dataset || [])
       .map(res => res.filter((data: Study) => data.id === id)[0] || null)
       .catch(ErrorService.handleError)
