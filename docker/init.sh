6e803d927e08db89b76bca081ab8c6ffe4983c20
Herculano Campos <hi@herkulano.com>
2017-07-12 14:54:35 +0300

    add support for base href config

--- docker/init.sh
+++ docker/init.sh
@@ -8,15 +8,27 @@ echo '==' && echo "==> Enviroment variables"
     API_URI=${API_URI:-'https://radar-cns.ddns.net/api'}
     echo API_URI=${API_URI}
 
-echo '==' && echo "==> Replace API_URI"
+    BASE_HREF=${BASE_HREF:-'/dashboard/'}
+    echo BASE_HREF=${BASE_HREF}
+
+echo '==' && echo "==> Replace API_URI & BASE_HREF"
     cd /var/www
 
-    # regex patterns
+    # file to replace
     FILE="index.html"
+
+    # regex patterns for API_URI
     FIND="(API_URI[^\"\']*)(.*)(,)"
     REPLACE="\\1\'${API_URI}\'\\3"
 
     # replace API_URI value
+    sed -ri "s|${FIND}|${REPLACE}|" ${FILE}
+
+    # regex patterns for BASE_HREF
+    FIND="(\<base\ href\=\")([^\"]+)"
+    REPLACE="\\1${BASE_HREF}"
+
+    # replace BASE_HREF value
     sed -ri "s|${FIND}|${REPLACE}|" ${FILE}
 
     # check replacement
