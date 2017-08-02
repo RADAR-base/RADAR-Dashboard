3cde3bde397103a5af086ecf904a42fcc2989140
Herculano Campos <hi@herkulano.com>
2017-08-02 14:39:58 +0300

    revert to subscription of route
    it’s needed in the template

--- src/app/pages/study/study.component.ts
+++ src/app/pages/study/study.component.ts
@@ -1,13 +1,13 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { Store } from '@ngrx/store'
-import 'rxjs/add/operator/withLatestFrom'
 import { Observable } from 'rxjs/Observable'
 
 import * as fromRoot from '../../shared/store/index'
 import * as studyAction from '../../shared/store/study/study.actions'
 import * as subjectAction from '../../shared/store/subject/subject.actions'
 import { Subject } from '../../shared/store/subject/subject.model'
+import { TakeUntilDestroy } from '../../shared/utils/TakeUntilDestroy'
 
 @Component({
   selector: 'app-study-page',
@@ -15,6 +15,7 @@ import { Subject } from '../../shared/store/subject/subject.model'
   styleUrls: ['./study.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+@TakeUntilDestroy
 export class StudyPageComponent implements OnInit {
 
   studyId: string
@@ -22,24 +23,30 @@ export class StudyPageComponent implements OnInit {
   isLoaded$: Observable<boolean>
   subjects$: Observable<Subject[]>
 
+  private takeUntilDestroy // from TakeUntilDestroy
+
   constructor (
     private route: ActivatedRoute,
     private store: Store<fromRoot.State>
   ) {}
 
   ngOnInit () {
+    // Get `studyId` from route and then get Study by `studyId`
+    this.route.params
+      .takeUntil(this.takeUntilDestroy())
+      .subscribe(params => {
+        this.studyId = params['studyId']
+      })
 
     // If study is not loaded and not valid then fetch it
     this.isStudyLoadedAndValid$ = this.store.select(fromRoot.getStudyIsLoadedAndValid)
-      .withLatestFrom(this.route.params)
-      .do(([isLoadedAndValid, params]) => {
+      .do(isLoadedAndValid => {
         if (isLoadedAndValid) {
-          this.store.dispatch(new subjectAction.GetAll(params.studyId))
+          this.store.dispatch(new subjectAction.GetAll(this.studyId))
         } else {
-          this.store.dispatch(new studyAction.GetById(params.studyId))
+          this.store.dispatch(new studyAction.GetById(this.studyId))
         }
       })
-      .map(([isLoadedAndValid, params]) => isLoadedAndValid)
       .publishReplay().refCount()
 
     // Check if study is loaded
