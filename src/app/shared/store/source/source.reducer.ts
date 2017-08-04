b081f37e61dd6a3cebdb6a5b9e80020d231ef14b
Herculano Campos <hi@herkulano.com>
2017-08-04 16:09:12 +0300

    migrate to ngrx@4
    * use ngrx createSelector

--- src/app/shared/store/source/source.reducer.ts
+++ src/app/shared/store/source/source.reducer.ts
@@ -1,4 +1,4 @@
-import { createSelector } from 'reselect'
+import { createSelector } from '@ngrx/store'
 
 import * as sourceActions from './source.actions'
 import { Source } from './source.model'
--- src/app/shared/store/study/study.reducer.ts
+++ src/app/shared/store/study/study.reducer.ts
@@ -1,4 +1,4 @@
-import { createSelector } from 'reselect'
+import { createSelector } from '@ngrx/store'
 
 import * as studyActions from './study.actions'
 import { Study } from './study.model'
