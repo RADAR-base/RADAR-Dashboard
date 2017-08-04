7f807b2833215fdf635a0c4246f766d3f3237d10
Herculano Campos <hi@herkulano.com>
2017-08-04 19:21:59 +0300

    migrate to ngrx@4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,10 @@
 import { DebugElement } from '@angular/core'
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
-import { StoreModule } from '@ngrx/store'
 
 import { AppComponent } from './app.component'
 import { AppModule } from './app.module'
 import { routes } from './app.routing'
-import { reducer } from './shared/store'
 
 describe('AppComponent', () => {
   let component: AppComponent
@@ -18,8 +16,7 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         AppModule,
-        RouterTestingModule.withRoutes(routes),
-        StoreModule.provideStore(reducer)
+        RouterTestingModule.withRoutes(routes)
       ]
     })
 
