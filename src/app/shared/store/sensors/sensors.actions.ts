3054edff77a31643019d0f13f4434e2b78237cf1
Herculano Campos <hi@herkulano.com>
2017-09-07 20:40:37 +0300

    simplify effects
    * remove passing around subjectId in the payload
    * add withLatestFrom to select values from store where needed for clarity
    * remove multi or single type check in the effects

--- src/app/shared/store/sensors/sensors.actions.ts
+++ src/app/shared/store/sensors/sensors.actions.ts
@@ -1,37 +1,52 @@
 import { Action } from '@ngrx/store'
 
+import { MultiTimeSeries } from '../../models/multi-time-series.model'
+import { TimeFrame } from '../../models/time-frame.model'
+import { TimeSeries } from '../../models/time-series.model'
 import { Source } from '../source/source.model'
 import { Sensor } from './sensors.model'
+
+export interface SensorsPayload {
+  subjectId: number
+  data: Sensor[]
+}
 
 export const GET_ALL = '[Sensor] GET_ALL'
 export const GET_ALL_SUCCESS = '[Sensor] GET_ALL_SUCCESS'
 export const GET_ALL_DATA = '[Sensor] GET_ALL_DATA'
 export const GET_ALL_DATA_SUCCESS = '[Sensor] GET_ALL_DATA_SUCCESS'
+export const SET_TIME_FRAME = '[Sensor] SET_TIME_FRAME'
 export const DESTROY = '[Sensor] DESTROY'
 export const TOGGLE_VISIBILITY = '[Sensor] TOGGLE_VISIBILITY'
 
 export class GetAll implements Action {
   readonly type = GET_ALL
 
-  constructor(public payload: { subjectId: String; data: Source[] }) {}
+  constructor(public payload: Source[]) {}
 }
 
 export class GetAllSuccess implements Action {
   readonly type = GET_ALL_SUCCESS
 
-  constructor(public payload: { subjectId: String; data: Source[] }) {}
+  constructor(public payload: Source[]) {}
 }
 
 export class GetAllData implements Action {
   readonly type = GET_ALL_DATA
 
-  constructor(public payload: { subjectId: String; data: Sensor }) {}
+  constructor(public payload: Sensor) {}
 }
 
 export class GetAllDataSuccess implements Action {
   readonly type = GET_ALL_DATA_SUCCESS
 
-  constructor(public payload: { id: any; data: any }) {}
+  constructor(public payload: TimeSeries[] | MultiTimeSeries[]) {}
+}
+
+export class SetTimeFrame implements Action {
+  readonly type = SET_TIME_FRAME
+
+  constructor(public payload: TimeFrame) {}
 }
 
 export class Destroy implements Action {
@@ -49,5 +64,6 @@ export type Actions =
   | GetAllData
   | GetAllDataSuccess
   | GetAllSuccess
+  | SetTimeFrame
   | Destroy
   | ToggleVisibility
--- src/app/shared/store/sensors/sensors.effects.ts
+++ src/app/shared/store/sensors/sensors.effects.ts
@@ -6,7 +6,6 @@ import { Observable } from 'rxjs/Observable'
 import * as fromRoot from '../../../shared/store/index'
 import { AppConfig } from '../../utils/config'
 import * as actions from './sensors.actions'
-import { DataTypes } from './sensors.model'
 import { SensorsService } from './sensors.service'
 
 @Injectable()
@@ -16,54 +15,42 @@ export class SensorsEffects {
     .ofType<actions.GetAll>(actions.GET_ALL)
     .map(action => action.payload)
     .switchMap(payload => {
-      return this.sensorsService.getAll(payload.data).map(
-        res =>
-          new actions.GetAllSuccess({
-            subjectId: payload.subjectId,
-            data: res
-          })
-      )
+      return this.sensorsService
+        .addSpecsToSources(payload)
+        .map(d => new actions.GetAllSuccess(d))
     })
 
   @Effect()
   getAllData$ = this.actions$
     .ofType<actions.GetAllSuccess>(actions.GET_ALL_SUCCESS)
     .map(action => action.payload)
-    .withLatestFrom(this.store$)
-    .switchMap(([payload, store]) => {
-      const entities = store.sensors.entities
-      return Object.keys(entities).map(key => {
-        return new actions.GetAllData({
-          subjectId: payload.subjectId,
-          data: entities[key]
-        })
-      })
-    })
+    .withLatestFrom(this.store.select(fromRoot.getSensorsAll))
+    .switchMap(([payload, sensors]) =>
+      sensors.map(sensor => new actions.GetAllData(sensor))
+    )
 
   @Effect()
   getAllDataSuccess$: Observable<Action> = this.actions$
     .ofType<actions.GetAllData>(actions.GET_ALL_DATA)
     .map(action => action.payload)
-    .mergeMap(payload => {
-      const serviceMapFn = res =>
-        new actions.GetAllDataSuccess({
-          id: payload.data.id,
-          data: res
-        })
-
-      if (
-        AppConfig.config.sensors[payload.data.type].dataType ===
-        DataTypes.single
-      ) {
-        return this.sensorsService.getDataSingle(payload).map(serviceMapFn)
-      } else {
-        return this.sensorsService.getDataMulti(payload).map(serviceMapFn)
-      }
-    })
+    .withLatestFrom(
+      this.store.select(fromRoot.getSubjectSelectedId),
+      this.store.select(fromRoot.getSensorsTimeFrame)
+    )
+    .mergeMap(([payload, subjectId, timeFrame]) =>
+      this.sensorsService
+        .getData(
+          payload,
+          subjectId,
+          timeFrame,
+          AppConfig.config.sensors[payload.type].dataType
+        )
+        .map(d => new actions.GetAllDataSuccess(d))
+    )
 
   constructor(
     private actions$: Actions,
     private sensorsService: SensorsService,
-    private store$: Store<fromRoot.State>
+    private store: Store<fromRoot.State>
   ) {}
 }
