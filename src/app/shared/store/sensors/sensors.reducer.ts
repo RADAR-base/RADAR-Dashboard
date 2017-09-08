3c7975147b02e13706edd1329f9ee86b9939362c
Herculano Campos <hi@herkulano.com>
2017-09-08 22:09:07 +0300

    add actions and simplify code

--- src/app/shared/store/sensors/sensors.reducer.ts
+++ src/app/shared/store/sensors/sensors.reducer.ts
@@ -4,7 +4,7 @@ import { MultiTimeSeries } from '../../models/multi-time-series.model'
 import { TimeFrame } from '../../models/time-frame.model'
 import { TimeSeries } from '../../models/time-series.model'
 import * as sensorsActions from './sensors.actions'
-import { Sensor } from './sensors.model'
+import { DescriptiveStatistic, Sensor, TimeInterval } from './sensors.model'
 
 export interface State {
   ids: string[]
@@ -12,10 +12,11 @@ export interface State {
   isLoaded: boolean
   data: { [id: number]: TimeSeries[] | MultiTimeSeries[] }
   dataLoaded: { [id: number]: boolean }
-  dates: number[]
+  dates: Date[]
   tooltipIndex: number
   timeFrame: TimeFrame
-  pristine: boolean
+  timeInterval: TimeInterval
+  descriptiveStatistic: DescriptiveStatistic
 }
 
 const initialState: State = {
@@ -27,7 +28,8 @@ const initialState: State = {
   dates: [],
   tooltipIndex: -1,
   timeFrame: { start: null, end: null },
-  pristine: true
+  timeInterval: TimeInterval.TEN_SECOND,
+  descriptiveStatistic: DescriptiveStatistic.AVERAGE
 }
 
 export function reducer(
@@ -35,15 +37,14 @@ export function reducer(
   action: sensorsActions.Actions
 ): State {
   switch (action.type) {
-    case sensorsActions.GET_ALL: {
+    case sensorsActions.GET_SENSORS: {
       return {
         ...state,
-        isLoaded: false,
-        pristine: false
+        isLoaded: false
       }
     }
 
-    case sensorsActions.GET_ALL_SUCCESS: {
+    case sensorsActions.GET_SENSORS_SUCCESS: {
       let counter = 0
       const payload = action.payload
       const ids = []
@@ -74,34 +75,42 @@ export function reducer(
       }
     }
 
-    case sensorsActions.GET_ALL_DATA: {
+    case sensorsActions.UPDATE_DATES: {
+      const iterations =
+        (state.timeFrame.end - state.timeFrame.start) / state.timeInterval
+
+      const dates = []
+      for (let i = 0; i < iterations; i++) {
+        dates[i] = new Date(state.timeFrame.start + state.timeInterval * i)
+      }
+
+      return {
+        ...state,
+        dates
+      }
+    }
+
+    case sensorsActions.GET_SENSORS_DATA: {
       return {
         ...state,
         dataLoaded: {}
       }
     }
 
-    case sensorsActions.GET_ALL_DATA_SUCCESS: {
-      // const payload = action.payload
-      // const id = payload.id
-      // const data = action.payload
-
-      // console.table(action.payload)
-
-      // const sensorData = {}
-
-      // if (data[0]) {
-      //   dataWithIds = { ...state.data['values'], [id]: data.map(d =>
-      // d.value) } } else { dataWithIds = { ...state.data['values'], [id]: {
-      // keys: data.keys, values: data.values } } }  let dataWithDates = {} if
-      // (state.data['dates']) { dataWithDates = { dates: state.data['dates'],
-      // values: dataWithIds } } else { const dates = data.map(d => d.date)
-      // dataWithDates = { dates: dates, values: dataWithIds } }
+    case sensorsActions.GET_SENSORS_DATA_SUCCESS: {
+      const data = action.payload.data
+      const id = action.payload.sensor.id
 
       return {
-        ...state
-        // data: dataWithDates,
-        // dataLoaded: { ...state.dataLoaded, [id]: true }
+        ...state,
+        data: {
+          ...state.data,
+          [id]: data
+        },
+        dataLoaded: {
+          ...state.dataLoaded,
+          [id]: true
+        }
       }
     }
 
@@ -109,6 +118,20 @@ export function reducer(
       return {
         ...state,
         timeFrame: action.payload
+      }
+    }
+
+    case sensorsActions.SET_TIME_INTERVAL: {
+      return {
+        ...state,
+        timeInterval: action.payload
+      }
+    }
+
+    case sensorsActions.SET_DESCRIPTIVE_STATISTIC: {
+      return {
+        ...state,
+        descriptiveStatistic: action.payload
       }
     }
 
@@ -142,21 +165,16 @@ export const getIsDataLoaded = (state: State) => state.dataLoaded
 export const getIds = (state: State) => state.ids
 export const getEntities = (state: State) => state.entities
 export const getData = (state: State) => state.data
-export const getPristine = (state: State) => state.pristine
+export const getDates = (state: State) => state.dates
 export const getTimeFrame = (state: State) => state.timeFrame
+export const getTimeInterval = (state: State) => state.timeInterval
+export const getDescriptiveStatistic = (state: State) =>
+  state.descriptiveStatistic
 
-export const getLabels = createSelector(
+export const getSensors = createSelector(
   getEntities,
   getIds,
   (entities, ids) => {
-    return ids.map(id => entities[id].label)
+    return ids.map(id => entities[id])
   }
 )
-
-export const getAll = createSelector(getEntities, getIds, (entities, ids) => {
-  return ids.map(id => entities[id])
-})
-
-export const getAllData = createSelector(getData, getIds, (data, ids) => {
-  return data
-})
