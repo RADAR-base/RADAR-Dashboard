f3bfd9092f44828c7a391b01903b042d801f7a4d
Herculano Campos <hi@herkulano.com>
2017-09-08 22:07:30 +0300

    add actions

--- src/app/shared/store/sensors/sensors.actions.ts
+++ src/app/shared/store/sensors/sensors.actions.ts
@@ -4,49 +4,65 @@ import { MultiTimeSeries } from '../../models/multi-time-series.model'
 import { TimeFrame } from '../../models/time-frame.model'
 import { TimeSeries } from '../../models/time-series.model'
 import { Source } from '../source/source.model'
-import { Sensor } from './sensors.model'
+import { DescriptiveStatistic, Sensor, TimeInterval } from './sensors.model'
 
-export interface SensorsPayload {
-  subjectId: number
-  data: Sensor[]
-}
-
-export const GET_ALL = '[Sensor] GET_ALL'
-export const GET_ALL_SUCCESS = '[Sensor] GET_ALL_SUCCESS'
-export const GET_ALL_DATA = '[Sensor] GET_ALL_DATA'
-export const GET_ALL_DATA_SUCCESS = '[Sensor] GET_ALL_DATA_SUCCESS'
+export const GET_SENSORS = '[Sensor] GET_SENSORS'
+export const GET_SENSORS_SUCCESS = '[Sensor] GET_SENSORS_SUCCESS'
+export const UPDATE_DATES = '[Sensor] UPDATE_DATES'
+export const GET_SENSORS_DATA = '[Sensor] GET_SENSORS_DATA'
+export const GET_SENSORS_DATA_SUCCESS = '[Sensor] GET_SENSORS_DATA_SUCCESS'
 export const SET_TIME_FRAME = '[Sensor] SET_TIME_FRAME'
+export const SET_TIME_INTERVAL = '[Sensor] SET_TIME_INTERVAL'
+export const SET_DESCRIPTIVE_STATISTIC = '[Sensor] SET_DESCRIPTIVE_STATISTIC'
 export const DESTROY = '[Sensor] DESTROY'
 export const TOGGLE_VISIBILITY = '[Sensor] TOGGLE_VISIBILITY'
 
-export class GetAll implements Action {
-  readonly type = GET_ALL
+export class GetSensors implements Action {
+  readonly type = GET_SENSORS
 
   constructor(public payload: Source[]) {}
 }
 
-export class GetAllSuccess implements Action {
-  readonly type = GET_ALL_SUCCESS
+export class GetSensorsSuccess implements Action {
+  readonly type = GET_SENSORS_SUCCESS
 
   constructor(public payload: Source[]) {}
 }
 
-export class GetAllData implements Action {
-  readonly type = GET_ALL_DATA
+export class UpdateDates implements Action {
+  readonly type = UPDATE_DATES
+}
+
+export class GetSensorsData implements Action {
+  readonly type = GET_SENSORS_DATA
 
   constructor(public payload: Sensor) {}
 }
 
-export class GetAllDataSuccess implements Action {
-  readonly type = GET_ALL_DATA_SUCCESS
+export class GetSensorsDataSuccess implements Action {
+  readonly type = GET_SENSORS_DATA_SUCCESS
 
-  constructor(public payload: TimeSeries[] | MultiTimeSeries[]) {}
+  constructor(
+    public payload: { data: TimeSeries[] | MultiTimeSeries[], sensor: Sensor }
+  ) {}
 }
 
 export class SetTimeFrame implements Action {
   readonly type = SET_TIME_FRAME
 
   constructor(public payload: TimeFrame) {}
+}
+
+export class SetTimeInterval implements Action {
+  readonly type = SET_TIME_INTERVAL
+
+  constructor(public payload: TimeInterval) {}
+}
+
+export class SetDescriptiveStatistic implements Action {
+  readonly type = SET_DESCRIPTIVE_STATISTIC
+
+  constructor(public payload: DescriptiveStatistic) {}
 }
 
 export class Destroy implements Action {
@@ -60,10 +76,13 @@ export class ToggleVisibility implements Action {
 }
 
 export type Actions =
-  | GetAll
-  | GetAllData
-  | GetAllDataSuccess
-  | GetAllSuccess
+  | GetSensors
+  | GetSensorsSuccess
+  | UpdateDates
+  | GetSensorsData
+  | GetSensorsDataSuccess
   | SetTimeFrame
+  | SetTimeInterval
+  | SetDescriptiveStatistic
   | Destroy
   | ToggleVisibility
