aae84afeb5fa2594013c4b28223cbdc1404f0191
Herculano Campos <hi@herkulano.com>
2017-09-08 21:59:25 +0300

    refactor service

--- src/app/shared/services/error.service.spec.ts
+++ src/app/shared/services/error.service.spec.ts
@@ -1,12 +1,12 @@
+import { HttpClientModule } from '@angular/common/http'
 import { TestBed, inject } from '@angular/core/testing'
-import { HttpModule } from '@angular/http'
 
 import { ErrorService } from './error.service'
 
 describe('ErrorLoggerServiceService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpModule],
+      imports: [HttpClientModule],
       providers: [ErrorService]
     })
   })
