643231db0136c3f0b68a531b3f3102bfbd9363a8
Herculano Campos <hi@herkulano.com>
2017-09-08 22:09:57 +0300

    refactor

--- src/app/shared/models/multi-time-series.model.ts
+++ src/app/shared/models/multi-time-series.model.ts
@@ -1,7 +1,7 @@
-import { Key } from './config.model'
+import { ConfigKey } from './config.model'
 
 export interface MultiTimeSeries {
   values: { [key: string]: number[] }
-  keys: Key[]
+  keys?: ConfigKey[]
   dates?: Date[]
 }
