967c8b656769b8357af999333e9c69e4709d5a39
Herculano Campos <hi@herkulano.com>
2017-09-11 17:37:02 +0300

    add mock actions

--- src/app/shared/store/sensors/sensors.service.spec.ts
+++ src/app/shared/store/sensors/sensors.service.spec.ts
@@ -2,12 +2,16 @@ import { HttpClientModule } from '@angular/common/http'
 import { TestBed, inject } from '@angular/core/testing'
 
 import { SensorsService } from './sensors.service'
+import { Observable } from 'rxjs/Observable'
+import { provideMockActions } from '@ngrx/effects/testing'
 
 describe('SensorsService', () => {
+  const actions: Observable<any> = Observable.of()
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      providers: [SensorsService]
+      providers: [SensorsService, provideMockActions(() => actions)]
     })
   })
 
--- src/app/shared/store/source/source.service.spec.ts
+++ src/app/shared/store/source/source.service.spec.ts
@@ -2,12 +2,16 @@ import { HttpClientModule } from '@angular/common/http'
 import { TestBed, inject } from '@angular/core/testing'
 
 import { SourceService } from './source.service'
+import { Observable } from 'rxjs/Observable'
+import { provideMockActions } from '@ngrx/effects/testing'
 
 describe('SourceService', () => {
+  const actions: Observable<any> = Observable.of()
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      providers: [SourceService]
+      providers: [SourceService, provideMockActions(() => actions)]
     })
   })
 
