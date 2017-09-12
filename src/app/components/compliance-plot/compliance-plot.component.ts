8d9fb657a53d0378bde6f2128a6f1aa82793d17b
pauline <mpgxvii@gmail.com>
2017-09-12 15:53:09 +0100

    add compliance keys as input to chart

--- src/app/components/compliance-plot/compliance-plot.component.ts
+++ src/app/components/compliance-plot/compliance-plot.component.ts
@@ -1,16 +1,28 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit
+} from '@angular/core'
+
+import { AppConfig } from '../../shared/utils/config'
 
 @Component({
   selector: 'app-compliance-plot',
   template: `
-    <div *ngIf="isComplianceLoaded && data" class="chart">
-      <app-chart-base-multi-bar [chartData]="data"></app-chart-base-multi-bar>
+    <div *ngIf="isComplianceLoaded && data && keys" class="chart">
+      <app-chart-base-multi-bar [chartData]="data" [keys]="keys"></app-chart-base-multi-bar>
     </div>
   `,
   styleUrls: ['./compliance-plot.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CompliancePlotComponent {
+export class CompliancePlotComponent implements OnInit {
   @Input() data
   @Input() isComplianceLoaded
+  keys: any[]
+
+  ngOnInit() {
+    if (AppConfig.config) this.keys = AppConfig.config.compliance.keys
+  }
 }
