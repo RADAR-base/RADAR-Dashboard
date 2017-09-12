3a8f25ec862a6c8fefbaf16c337596e72021fa79
pauline <mpgxvii@gmail.com>
2017-09-12 15:50:06 +0100

    add/fix test data files

--- src/app/shared/testing/mocks/mock-compliance-data.ts
+++ src/app/shared/testing/mocks/mock-compliance-data.ts
@@ -1,14 +1,247 @@
 export const MockComplianceData = {
-  keys: [
-    { key: 'simple', label: { EN: '' } },
-    { key: 'special', label: { EN: '' } }
-  ],
-  dates: [
-    new Date('2016-10-27T20:02:20.000Z'),
-    new Date('2016-10-27T20:03:00.000Z')
-  ],
-  values: {
-    simple: [0.5, 0.1],
-    special: [0.23, 0.123]
-  }
+  header: {
+    subjectId: 'KCLTest0',
+    sourceId: '00:07:80:1F:52:F3',
+    source: 'EMPATICA',
+    sensor: 'COMPLIANCE',
+    descriptiveStatistic: 'AVERAGE',
+    unit: 'G',
+    timeFrame: 'ONE_DAY',
+    effectiveTimeFrame: {
+      startDateTime: '2017-02-28T11:46:00Z',
+      endDateTime: '2017-04-01T11:46:00Z'
+    }
+  },
+  dataset: [
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0
+      },
+      startDateTime: '2017-02-28T11:46:00Z'
+    },
+    {
+      sample: {
+        special: 0.823
+      },
+      startDateTime: '2017-03-01T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.3348,
+        special: 0.323
+      },
+      startDateTime: '2017-03-02T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-03T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-04T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-05T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-06T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-07T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-08T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-09T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-10T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-11T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-12T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-13T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-14T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-15T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-16T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-17T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-18T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-19T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-20T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-21T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-22T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-23T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-24T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-25T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-26T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-27T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-28T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-29T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-30T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-03-31T11:46:00Z'
+    },
+    {
+      sample: {
+        simple: 0.2348,
+        special: 0.123
+      },
+      startDateTime: '2017-04-01T11:46:00Z'
+    }
+  ]
 }
