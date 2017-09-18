bb573b8a4db77c00eb6dc074144ddd1b198722c6
Herculano Campos <hi@herkulano.com>
2017-09-18 11:51:25 +0300

    various improvements
    * set data attribute for tooltip-mouse-box because it's easier to target
    * add position check to the right to avoid hiding the tooltip
    * add value to component for easier maintenance
    * add styles
    * refactor names
    * clean code

--- src/app/components/source-graphs/source-graphs.component.ts
+++ src/app/components/source-graphs/source-graphs.component.ts
@@ -48,12 +48,15 @@ export class SourceGraphsComponent implements OnInit {
   }
 
   onMouseMove(event) {
-    if (event.target.classList.value === 'tooltip-box') {
-      // TODO: improve positioning on the right side so that it's not hidden
-      this.tooltipX = event.x - this.tooltip.width / 2
-      this.tooltipY = event.y
+    if (event.target.dataset.tooltipMouseBox) {
+      this.tooltipX =
+        event.clientX < document.body.clientWidth - this.tooltip.mid
+          ? event.clientX - this.tooltip.mid
+          : document.body.clientWidth - this.tooltip.width
+
+      this.tooltipY = event.clientY
+      this.lineX = event.layerX
       this.tooltipShow = 1
-      this.lineX = event.layerX + 8
     } else {
       this.tooltipShow = 0
     }
