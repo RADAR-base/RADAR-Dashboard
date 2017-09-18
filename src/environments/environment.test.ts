b3f123550f4dfef374c8e9540efb19430964807a
Herculano Campos <hi@herkulano.com>
2017-09-18 14:27:05 +0300

    change API_URI in test environment

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -2,7 +2,7 @@ export const ENV = {
   PROD: false,
   TOOLS: false,
   PARAMS: {
-    API_URI: 'API',
+    API_URI: '/api',
     API_LOCAL: 'assets/data',
     API_FIREBASE: 'https://radar-dashboard.firebaseio.com'
   }
