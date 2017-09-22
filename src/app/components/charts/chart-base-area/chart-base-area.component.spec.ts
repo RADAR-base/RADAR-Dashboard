df4981817c7817ceca670d14b8407fd9b7eb2233
pauline <mpgxvii@gmail.com>
2017-09-22 14:12:28 +0100

    add area chart test

--- src/app/components/charts/chart-base-area/chart-base-area.component.spec.ts
+++ src/app/components/charts/chart-base-area/chart-base-area.component.spec.ts
@@ -1,6 +1,12 @@
 import { DebugElement } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
+import {
+  MockAPISampleDataset,
+  MockTimeFrameChartData,
+  MockTimeIntervalChartData
+} from '../../../shared/testing/mocks/mock-chart-data'
+import { parseTimeHoles } from '../../../shared/utils/parse-time-holes'
 import { ChartBaseAreaComponent } from './chart-base-area.component'
 
 describe('ChartBaseAreaComponent', () => {
@@ -22,7 +28,68 @@ describe('ChartBaseAreaComponent', () => {
     fixture.detectChanges()
   })
 
-  it('should be created', () => {
-    expect(component).toBeTruthy()
+  describe('=> without @Input', () => {
+    beforeEach(() => {
+      fixture.detectChanges()
+    })
+
+    it('should create', () => {
+      expect(component).toBeTruthy()
+    })
+
+    it('should create a chart', () => {
+      expect(element.querySelector('g.chart')).toBeTruthy()
+    })
+
+    it('should update() and change size if data changes', () => {
+      // without data
+      expect(component.width).toBeFalsy()
+      expect(component.height).toBeFalsy()
+      expect(component.chartData).toBeFalsy()
+    })
+
+    it('should have area property when data changes', () => {
+      // without data
+      expect(element.querySelector('g.chart.area')).toBeFalsy()
+    })
+  })
+
+  describe('=> with @Input and hasXAxis & hasYAxis', () => {
+    beforeEach(() => {
+      component.chartData = parseTimeHoles(
+        MockAPISampleDataset,
+        MockTimeFrameChartData,
+        MockTimeIntervalChartData
+      )
+      component.hasYAxis = true
+      component.hasXAxis = true
+      fixture.detectChanges()
+    })
+
+    it('should create', () => {
+      expect(component).toBeTruthy()
+    })
+
+    it('should create a chart', () => {
+      expect(element.querySelector('g.chart')).toBeTruthy()
+    })
+
+    it('should update() and change size if data changes', () => {
+      // with data
+      expect(component.width).toBeGreaterThan(0)
+      expect(component.height).toBeGreaterThan(0)
+    })
+
+    it('area should have attribute d when data changes', () => {
+      // with data
+      // select element again as they'll be instantiated
+      expect(element.querySelector('.area')).toBeTruthy()
+
+      const areaElement: any = element.querySelectorAll('.area')
+
+      Object.getOwnPropertyNames(areaElement).forEach(prop => {
+        expect(areaElement[prop].getAttribute('d')).toBeTruthy()
+      })
+    })
   })
 })
