3959f1601df9da0783c010a3e0e06020e7cab113
pauline <mpgxvii@gmail.com>
2017-09-26 23:43:14 +0100

    fix overview state selectors

--- src/app/pages/overview/store/index.ts
+++ src/app/pages/overview/store/index.ts
@@ -2,13 +2,22 @@ import { createFeatureSelector, createSelector } from '@ngrx/store'
 
 import * as fromStudy from './study/study.reducer'
 
-export interface State {
+export interface StudyState {
   study: fromStudy.State
 }
 
-export const reducers = fromStudy.reducer
+export interface State {
+  study: StudyState
+}
 
-export const getStudyState = createFeatureSelector<fromStudy.State>('overview')
+export const reducers = { study: fromStudy.reducer }
+
+export const getOverviewState = createFeatureSelector<StudyState>('overview')
+
+export const getStudyState = createSelector(
+  getOverviewState,
+  state => state.study
+)
 
 export const getStudiesLoaded = createSelector(
   getStudyState,
