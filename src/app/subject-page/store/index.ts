a8bb6f5619ab43cce08922d6c8bdae53e347ebb6
pauline <mpgxvii@gmail.com>
2017-11-16 17:57:48 +0000

    fix lint errors

--- src/app/subject-page/store/index.ts
+++ src/app/subject-page/store/index.ts
@@ -6,154 +6,154 @@ import * as fromSources from './sources/sources.reducer'
 import * as fromSubject from './subject/subject.reducer'
 
 export interface State {
-    subject: fromSubject.State
-    sources: fromSources.State
-    sensors: fromSensors.State
-    sensorsData: fromSensorsData.State
+  subject: fromSubject.State
+  sources: fromSources.State
+  sensors: fromSensors.State
+  sensorsData: fromSensorsData.State
 }
 
 export const reducers = {
-    subject: fromSubject.reducer,
-    sources: fromSources.reducer,
-    sensors: fromSensors.reducer,
-    sensorsData: fromSensorsData.reducer
+  subject: fromSubject.reducer,
+  sources: fromSources.reducer,
+  sensors: fromSensors.reducer,
+  sensorsData: fromSensorsData.reducer
 }
 
 export const getSubjectPageState = createFeatureSelector<State>('subjectPage')
 
 // Subject Selectors
 export const getSubjectState = createSelector(
-    getSubjectPageState,
-    state => state.subject
+  getSubjectPageState,
+  state => state.subject
 )
 
 export const getStudyId = createSelector(
-    getSubjectState,
-    fromSubject.getStudyId
+  getSubjectState,
+  fromSubject.getStudyId
 )
 
 export const getSubjectId = createSelector(getSubjectState, fromSubject.getId)
 
 // Sensors Selectors
 export const getSensorsState = createSelector(
-    getSubjectPageState,
-    state => state.sensors
+  getSubjectPageState,
+  state => state.sensors
 )
 export const {
   selectEntities: getSensorsEntities,
-    selectAll: getSensors
+  selectAll: getSensors
 } = fromSensors.adapter.getSelectors(getSensorsState)
 
 export const getSensorsLoaded = createSelector(
-    getSensorsState,
-    fromSensors.getIsLoaded
+  getSensorsState,
+  fromSensors.getIsLoaded
 )
 
 // Sources Selectors
 export const getSourcesState = createSelector(
-    getSubjectPageState,
-    state => state.sources
+  getSubjectPageState,
+  state => state.sources
 )
 export const { selectAll: getSources } = fromSources.adapter.getSelectors(
-    getSourcesState
+  getSourcesState
 )
 
 export const getSourcesLoaded = createSelector(
-    getSourcesState,
-    fromSources.getIsLoaded
+  getSourcesState,
+  fromSources.getIsLoaded
 )
 
 export const getSourcesWithSensors = createSelector(
-    getSources,
-    getSensors,
-    (sources, sensors) => {
-        if (sensors.length) {
-            const sensorsBySource = sources.reduce((acc, source) => {
-                return { ...acc, [source.id]: [] }
-            }, {})
+  getSources,
+  getSensors,
+  (sources, sensors) => {
+    if (sensors.length) {
+      const sensorsBySource = sources.reduce((acc, source) => {
+        return { ...acc, [source.id]: [] }
+      }, {})
 
-            sensors.map(sensor => {
-                sensorsBySource[sensor.source].push(sensor)
-            })
+      sensors.map(sensor => {
+        sensorsBySource[sensor.source].push(sensor)
+      })
 
-            return sources.map(source => {
-                return { ...source, sensors: sensorsBySource[source.id] }
-            })
-        }
+      return sources.map(source => {
+        return { ...source, sensors: sensorsBySource[source.id] }
+      })
     }
+  }
 )
 
 // Sensors Data Selectors
 export const getSensorsDataState = createSelector(
-    getSubjectPageState,
-    state => state.sensorsData
+  getSubjectPageState,
+  state => state.sensorsData
 )
 export const {
   selectIds: getSensorsDataIds,
-    selectEntities: getSensorsDataEntities,
-    selectAll: getSensorsData
+  selectEntities: getSensorsDataEntities,
+  selectAll: getSensorsData
 } = fromSensorsData.adapter.getSelectors(getSensorsDataState)
 
 export const getSensorsDataLoaded = createSelector(
-    getSensorsDataState,
-    fromSensorsData.getIsDataLoaded
+  getSensorsDataState,
+  fromSensorsData.getIsDataLoaded
 )
 
 export const getSensorsDates = createSelector(
-    getSensorsDataState,
-    fromSensorsData.getDates
+  getSensorsDataState,
+  fromSensorsData.getDates
 )
 
 export const getTooltipDate = createSelector(
-    getSensorsDataState,
-    fromSensorsData.getTooltipDate
+  getSensorsDataState,
+  fromSensorsData.getTooltipDate
 )
 
 export const getSensorsTimeFrame = createSelector(
-    getSensorsDataState,
-    fromSensorsData.getTimeFrame
+  getSensorsDataState,
+  fromSensorsData.getTimeFrame
 )
 
 export const getSensorsTimeInterval = createSelector(
-    getSensorsDataState,
-    fromSensorsData.getTimeInterval
+  getSensorsDataState,
+  fromSensorsData.getTimeInterval
 )
 
 export const getSensorsDescriptiveStatistic = createSelector(
-    getSensorsDataState,
-    fromSensorsData.getDescriptiveStatistic
+  getSensorsDataState,
+  fromSensorsData.getDescriptiveStatistic
 )
 
 export const getSensorsTooltipValues = createSelector(
-    getSensorsDataIds,
-    getSensorsEntities,
-    getSensorsDataEntities,
-    getTooltipDate,
-    (ids: (number | string)[], sensors, sensorsEntities, date) => {
-        if (!date) return []
+  getSensorsDataIds,
+  getSensorsEntities,
+  getSensorsDataEntities,
+  getTooltipDate,
+  (ids: (number | string)[], sensors, sensorsEntities, date) => {
+    if (!date) return []
 
-        ids = ids.filter(d => sensors[d].visible == true)
+    ids = ids.filter(d => sensors[d].visible)
 
-        return ids.reduce((acc, id) => {
-            const index =
-                sensorsEntities[id].data &&
-                sensorsEntities[id].data.findIndex(
-                    d => d.date.getTime() === date.getTime()
-                )
+    return ids.reduce((acc, id) => {
+      const index =
+        sensorsEntities[id].data &&
+        sensorsEntities[id].data.findIndex(
+          d => d.date.getTime() === date.getTime()
+        )
 
-            return [
-                ...acc,
-                {
-                    id: id,
-                    label: sensors[id].label,
-                    dataType: sensors[id].dataType,
-                    keys: sensors[id].keys || null,
-                    value:
-                    sensorsEntities[id] && index > -1
-                        ? sensorsEntities[id].data[index].value
-                        : null
-                }
-            ]
-        }, [])
-    }
+      return [
+        ...acc,
+        {
+          id: id,
+          label: sensors[id].label,
+          dataType: sensors[id].dataType,
+          keys: sensors[id].keys || null,
+          value:
+            sensorsEntities[id] && index > -1
+              ? sensorsEntities[id].data[index].value
+              : null
+        }
+      ]
+    }, [])
+  }
 )
