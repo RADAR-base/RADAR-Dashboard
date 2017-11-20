742451dc69134e6601c505d06d5211209c7061e6
Herculano Campos <hi@herkulano.com>
2017-11-20 17:51:16 +0200

    simplified zero value test

--- src/app/shared/testing/mocks/mock-chart-data.ts
+++ src/app/shared/testing/mocks/mock-chart-data.ts
@@ -4,9 +4,9 @@ import { ChartData } from '../../models/chart-data.model'
 export const MockAPISampleDataset = [
   { startDateTime: '2017-06-16T10:09:10.000Z', sample: { value: 153 } },
   { startDateTime: '2017-06-16T10:09:50.000Z', sample: { value: 87 } },
-  { startDateTime: '2017-06-16T10:14:10.000Z', sample: { value: 150 } },
+  { startDateTime: '2017-06-16T10:14:10.000Z', sample: { value: 0 } },
   { startDateTime: '2017-06-16T10:14:20.000Z', sample: { value: 143 } },
-  { startDateTime: '2017-06-16T10:14:30.000Z', sample: { value: 129 } },
+  { startDateTime: '2017-06-16T10:14:30.000Z', sample: { value: 0 } },
   { startDateTime: '2017-06-16T10:46:40.000Z', sample: { value: 98 } },
   { startDateTime: '2017-06-16T10:46:50.000Z', sample: { value: 101 } },
   { startDateTime: '2017-06-16T10:49:00.000Z', sample: { value: 159 } }
@@ -25,9 +25,9 @@ export const MockChartDataExpected: ChartData[] = [
   { date: new Date('2017-06-16T10:09:40.000Z'), value: null },
   { date: new Date('2017-06-16T10:09:50.000Z'), value: 87 },
   { date: new Date('2017-06-16T10:14:00.000Z'), value: null },
-  { date: new Date('2017-06-16T10:14:10.000Z'), value: 150 },
+  { date: new Date('2017-06-16T10:14:10.000Z'), value: 0 },
   { date: new Date('2017-06-16T10:14:20.000Z'), value: 143 },
-  { date: new Date('2017-06-16T10:14:30.000Z'), value: 129 },
+  { date: new Date('2017-06-16T10:14:30.000Z'), value: 0 },
   { date: new Date('2017-06-16T10:46:30.000Z'), value: null },
   { date: new Date('2017-06-16T10:46:40.000Z'), value: 98 },
   { date: new Date('2017-06-16T10:46:50.000Z'), value: 101 },
@@ -47,30 +47,4 @@ export const MockChartDataWithStartEndHolesExpected: ChartData[] = [
   },
   ...MockChartDataExpected,
   { date: new Date(MockTimeFrameChartDataWithStartEndHoles.end), value: null }
-]
-
-export const MockAPISampleDatasetZeroVals = [
-  { startDateTime: '2017-06-16T10:09:10.000Z', sample: { value: 0 } },
-  { startDateTime: '2017-06-16T10:09:50.000Z', sample: { value: 0 } },
-  { startDateTime: '2017-06-16T10:14:10.000Z', sample: { value: 0 } },
-  { startDateTime: '2017-06-16T10:14:20.000Z', sample: { value: 0 } },
-  { startDateTime: '2017-06-16T10:14:30.000Z', sample: { value: 0 } },
-  { startDateTime: '2017-06-16T10:46:40.000Z', sample: { value: 0 } },
-  { startDateTime: '2017-06-16T10:46:50.000Z', sample: { value: 0 } },
-  { startDateTime: '2017-06-16T10:49:00.000Z', sample: { value: 0 } }
-]
-
-export const MockChartDataZeroValsExpected: ChartData[] = [
-  { date: new Date('2017-06-16T10:09:10.000Z'), value: 0 },
-  { date: new Date('2017-06-16T10:09:40.000Z'), value: null },
-  { date: new Date('2017-06-16T10:09:50.000Z'), value: 0 },
-  { date: new Date('2017-06-16T10:14:00.000Z'), value: null },
-  { date: new Date('2017-06-16T10:14:10.000Z'), value: 0 },
-  { date: new Date('2017-06-16T10:14:20.000Z'), value: 0 },
-  { date: new Date('2017-06-16T10:14:30.000Z'), value: 0 },
-  { date: new Date('2017-06-16T10:46:30.000Z'), value: null },
-  { date: new Date('2017-06-16T10:46:40.000Z'), value: 0 },
-  { date: new Date('2017-06-16T10:46:50.000Z'), value: 0 },
-  { date: new Date('2017-06-16T10:48:50.000Z'), value: null },
-  { date: new Date('2017-06-16T10:49:00.000Z'), value: 0 }
 ]
--- src/app/shared/utils/parse-time-holes.spec.ts
+++ src/app/shared/utils/parse-time-holes.spec.ts
@@ -1,10 +1,8 @@
 import { TimeInterval } from '../enums/time-interval.enum'
 import {
   MockAPISampleDataset,
-  MockAPISampleDatasetZeroVals,
   MockChartDataExpected,
   MockChartDataWithStartEndHolesExpected,
-  MockChartDataZeroValsExpected,
   MockTimeFrameChartData,
   MockTimeFrameChartDataWithStartEndHoles
 } from '../testing/mocks/mock-chart-data'
@@ -18,15 +16,6 @@ import {
 import { parseTimeHoles } from './parse-time-holes'
 
 describe('parseTimeHoles', () => {
-  it('Sensor Data > 10s Interval > Zero Values', () => {
-    const actual = parseTimeHoles(
-      MockAPISampleDatasetZeroVals,
-      MockTimeFrameChartData,
-      TimeInterval.TEN_SECOND
-    )
-    expect(actual).toEqual(MockChartDataZeroValsExpected)
-  })
-
   it('Sensor Data > 10s Interval > TimeFrame === Data', () => {
     const actual = parseTimeHoles(
       MockAPISampleDataset,
