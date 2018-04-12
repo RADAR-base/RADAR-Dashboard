1a9a072d081ab24fafb164373feda785b7f66fc6
Herculano Campos <hi@herkulano.com>
2018-04-12 17:00:20 +0300

    Update API endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const ENV = {
   TEST: false,
   TOOLS: false,
   PARAMS: {
-    API_URI: 'https://radar-cns.ddns.net/api',
+    API_URI: 'https://radar-backend.co.uk/api',
     API_LOCAL: 'assets/data',
     API_FIREBASE: 'https://radar-dashboard.firebaseio.com'
   }
