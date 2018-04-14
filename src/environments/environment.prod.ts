85850126ff7360c115d693c0c5a8d30b13dd59a3
Herculano Campos <hi@herkulano.com>
2018-04-14 14:00:59 +0300

    Add auth config
    #282

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,12 @@ export const ENV = {
     API_URI: 'https://radar-backend.co.uk/api',
     API_LOCAL: 'assets/data',
     API_FIREBASE: 'https://radar-dashboard.firebaseio.com'
+  },
+  AUTH_URI: 'https://radar-backend.co.uk/managementportal/oauth/token',
+  AUTH: {
+    grant_type: 'client_credentials',
+    client_id: 'radar_dashboard',
+    client_secret: 'says_PRETTY_smiled',
+    scope: 'SOURCETYPE.READ PROJECT.READ SOURCE.READ SUBJECT.READ'
   }
 }
