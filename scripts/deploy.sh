aa862e6da44d6f69918b5fd2421917839832c4c9
Herculano Campos <hi@herkulano.com>
2018-05-12 20:26:00 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 BRANCH=$1
-
-now -t $NOW_TOKEN --public --docker --name radar-dasboard-$BRANCH \
-  && now -t $NOW_TOKEN alias radar-dasboard-$BRANCH \
-  && now -t $NOW_TOKEN remove radar-dasboard-$BRANCH --safe
+ID=$(now -t $NOW_TOKEN --public --docker --name radar-dasboard-$BRANCH)
+now -t $NOW_TOKEN alias $ID radar-dasboard-$BRANCH
+now -t $NOW_TOKEN remove radar-dasboard-$BRANCH --safe -y
 
 exit 0
