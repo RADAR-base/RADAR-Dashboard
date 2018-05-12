e8f1c9db91a5ec61edc74ef6127b97d4f864b866
Herculano Campos <hi@herkulano.com>
2018-05-12 19:12:34 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 BRANCH=$1
 
-now --name radar-dasboard-$BRANCH --token $NOW_TOKEN
+now --public --docker --name radar-dasboard-$BRANCH --token $NOW_TOKEN
 now radar-dasboard-$BRANCH --token $NOW_TOKEN
 
 exit 0
