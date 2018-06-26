e7dc623d30b4434999ee98a3a68b8b3de1da0a9e
Herculano Campos <hi@herkulano.com>
2018-06-26 17:14:37 +0300

    Fix test

--- src/app/study/components/subject-detail/subject-detail.component.spec.ts
+++ src/app/study/components/subject-detail/subject-detail.component.spec.ts
@@ -1,25 +1,29 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing'
+import { RouterTestingModule } from '@angular/router/testing'
 
-import { SubjectDetailComponent } from './subject-detail.component';
+import { SubjectDetailComponent } from './subject-detail.component'
 
 describe('SubjectDetailComponent', () => {
-  let component: SubjectDetailComponent;
-  let fixture: ComponentFixture<SubjectDetailComponent>;
+  let component: SubjectDetailComponent
+  let fixture: ComponentFixture<SubjectDetailComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SubjectDetailComponent ]
-    })
-    .compileComponents();
-  }));
+      imports: [RouterTestingModule],
+      declarations: [SubjectDetailComponent]
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SubjectDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(SubjectDetailComponent)
+    component = fixture.componentInstance
+
+    component.subject = null
+
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
