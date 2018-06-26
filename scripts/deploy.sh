ec753a012fb7aabe1623bef1487615f246202470
Herculano Campos <hi@herkulano.com>
2018-06-26 11:41:53 +0300

    Remove now deployments
    Using netlify instead as it’s automated and requires no scripts or maintenance.

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BRANCH=$1
-ID=$(now -t $NOW_TOKEN --public --docker --name radar-dasboard-$BRANCH)
-now -t $NOW_TOKEN alias $ID radar-dasboard-$BRANCH
-now -t $NOW_TOKEN remove radar-dasboard-$BRANCH --safe -y
-
-exit 0
