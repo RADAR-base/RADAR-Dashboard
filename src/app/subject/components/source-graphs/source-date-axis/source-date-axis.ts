de1682f54b2c7fd9284aaf170045836f0cf29485
pauline <mpgxvii@gmail.com>
2018-07-09 13:52:42 +0100

    fix bottom div margins

--- src/app/subject/components/source-graphs/source-date-axis/source-date-axis.ts
+++ src/app/subject/components/source-graphs/source-date-axis/source-date-axis.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core'
       *ngIf="dates[0]"
       [chartData]="dates"
       [hasXAxis]="true"
-      [margin]="{top: 0, right: 36, bottom: 0, left: 48 }"
+      [margin]="{top: 0, right: 36, bottom: 0, left: 80 }"
     ></app-chart-date-axis>
   `,
   styleUrls: ['./source-date-axis.scss']
