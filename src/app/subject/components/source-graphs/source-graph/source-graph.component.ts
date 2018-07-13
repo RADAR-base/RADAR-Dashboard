53b4097615dd9ea591788cc62bf919207d5cc185
pauline <mpgxvii@gmail.com>
2018-07-13 11:17:50 +0100

    fix fix linechunked not showing in safari

--- src/app/subject/components/source-graphs/source-graph/source-graph.component.ts
+++ src/app/subject/components/source-graphs/source-graph/source-graph.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 import { Store } from '@ngrx/store'
+import { Observable } from 'rxjs'
 
 import { AppConfig } from '../../../../shared/app-config'
 import { DataType } from '../../../../shared/enums/data-type.enum'
 import { ChartData } from '../../../../shared/models/chart-data.model'
 import { SourceData } from '../../../../shared/models/source-data.model'
+import * as fromRoot from '../../../../store'
 import * as fromSubject from '../../../store'
 import * as sensorsDataActions from '../../../store/sensors-data/sensors-data.actions'
 
@@ -31,6 +33,7 @@ import * as sensorsDataActions from '../../../store/sensors-data/sensors-data.ac
       [hasYAxis]="true"
       [hasTooltip]="true"
       [margin]="graphMargins"
+      [path]="path$ | async"
       (tooltipMouseMove)="onTooltipMouseMove($event)"
     ></app-chart-base-line>
 
@@ -42,6 +45,7 @@ import * as sensorsDataActions from '../../../store/sensors-data/sensors-data.ac
       [hasYAxis]="true"
       [hasTooltip]="true"
       [margin]="graphMargins"
+      [path]="path$ | async"
       (tooltipMouseMove)="onTooltipMouseMove($event)"
     ></app-chart-base-multi-line>
   `,
@@ -54,6 +58,7 @@ export class SourceGraphComponent {
   @Input() sensorData: ChartData
   @Input() sourceData: SourceData
 
+  path$: Observable<any>
   graphMargins = { top: 32, right: 16, bottom: 32, left: 80 }
 
   get keys() {
@@ -76,7 +81,12 @@ export class SourceGraphComponent {
     return this.sourceData.label && this.sourceData.label[AppConfig.language]
   }
 
-  constructor(private store: Store<fromSubject.State>) {}
+  constructor(
+    private store: Store<fromSubject.State>,
+    private rootStore: Store<fromRoot.State>
+  ) {
+    this.path$ = this.rootStore.select(fromRoot.getRouterUrl)
+  }
 
   onTooltipMouseMove(date: Date) {
     this.store.dispatch(new sensorsDataActions.SetTooltipDate(date))
