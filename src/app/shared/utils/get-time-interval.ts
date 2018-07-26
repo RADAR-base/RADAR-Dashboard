a82e376b45da1707a86def4747ae5a62fb19b09d
pauline <mpgxvii@gmail.com>
2018-07-26 13:04:36 +0100

    fix time interval function according to limit

--- src/app/shared/utils/get-time-interval.ts
+++ src/app/shared/utils/get-time-interval.ts
@@ -3,21 +3,26 @@ import { TimeFrame } from '../models/time.model'
 
 export function getTimeInterval(timeFrame: TimeFrame) {
   const ONE_YEAR = TimeWindow['ONE_WEEK'] * 52
-  const THREE_MONTHS = ONE_YEAR / 4
-  const TWO_DAYS = (ONE_YEAR / 365) * 2
-  const TWO_WEEKS = TimeWindow['ONE_WEEK'] * 2
+  const TWO_MONTHS = ONE_YEAR / 6
+  const ONE_DAY = TimeWindow['ONE_DAY']
+  const FORTY_DAYS = ONE_DAY * 40
+  const TWO_HOURS = TimeWindow['ONE_HOUR'] * 2
+  const SIXTEEN_HOURS = TWO_HOURS * 8
+  const SIXTEEN_MINUTES = TimeWindow['ONE_MIN'] * 16
 
   const difference =
     timeFrame.endDateTime.getTime() - timeFrame.startDateTime.getTime()
 
   switch (true) {
-    case difference <= THREE_MONTHS && difference >= TWO_WEEKS:
+    case difference <= FORTY_DAYS &&
+      difference >= TimeWindow['ONE_WEEK'] - TWO_HOURS:
       return 'ONE_HOUR'
-    case difference < TWO_WEEKS && difference >= TWO_DAYS:
+    case difference < TimeWindow['ONE_WEEK'] - TWO_HOURS &&
+      difference >= SIXTEEN_HOURS:
       return 'TEN_MIN'
-    case difference < TWO_DAYS && difference >= TimeWindow['ONE_HOUR']:
+    case difference < SIXTEEN_HOURS && difference >= SIXTEEN_MINUTES:
       return 'ONE_MIN'
-    case difference < TimeWindow['ONE_HOUR'] && difference > 0:
+    case difference < SIXTEEN_MINUTES && difference > 0:
       return 'TEN_SECOND'
     default:
       return 'ONE_DAY'
