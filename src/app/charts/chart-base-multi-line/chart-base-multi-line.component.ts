1bf6d6caef1974fba5a362a596c3976ae7d1ebc3
Jm Conde <jm@Jms-MacBook-Pro.local>
2018-08-07 11:00:46 +0800

    add legend multiline

--- src/app/charts/chart-base-multi-line/chart-base-multi-line.component.ts
+++ src/app/charts/chart-base-multi-line/chart-base-multi-line.component.ts
@@ -18,6 +18,7 @@ export class ChartBaseMultiLineComponent extends ChartBaseComponent {
   lines: any
   line: any
   lineGroup: any
+  legend
 
   init() {
     this.lineGroup = this.chart.append('g')
@@ -83,5 +84,62 @@ export class ChartBaseMultiLineComponent extends ChartBaseComponent {
         .attr('opacity', 1)
         .duration(500)
     })
+
+    this.legend = this.chart
+      .append('g')
+      .attr('class', 'legend')
+      .append('svg')
+      .attr('x', this.width - 250)
+      .attr('y', -20)
+      .attr('width', this.width)
+      .attr('height', this.height - 100)
+
+    let dataL = 0
+    const offset = 80
+
+    this.legend
+      .append('rect')
+      .attr('width', '25%')
+      .attr('height', '20%')
+      .attr('fill', '#0b4a59')
+      .attr('rx', '4')
+      .attr('ry', '4')
+      .attr('fill-opacity', 0.2)
+      .attr('class', 'legend')
+
+    this.legend = this.legend
+      .selectAll('.legend')
+      .data(this.keys.map(d => d.label.EN))
+      .enter()
+      .append('g')
+      .attr('class', 'legend')
+      .attr('margin', 10)
+      .attr('transform', function(d, i) {
+        if (i === 0) {
+          dataL = d.length + offset
+          return 'translate(0,5)'
+        } else {
+          const newdataL = dataL
+          dataL += d.length + offset
+          return 'translate(' + newdataL + ',5)'
+        }
+      })
+
+    this.legend
+      .append('circle')
+      .attr('cx', 5)
+      .attr('cy', 5)
+      .attr('r', 4)
+      .style('fill', (d, i) => this.colorScale(d.key))
+
+    this.legend
+      .append('text')
+      .attr('x', 20)
+      .attr('y', 10)
+      .text(d => d)
+      .attr('class', 'text')
+      .style('text-anchor', 'start')
+      .style('fill', 'white')
+      .attr('font-size', 10)
   }
 }
