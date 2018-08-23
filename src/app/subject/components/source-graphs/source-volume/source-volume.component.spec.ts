7720ae9e82b0e93b5ac97c5353fe73862fc62030
pauline <mpgxvii@gmail.com>
2018-08-23 16:58:34 +0100

    add tests in volume component (brush)

--- src/app/subject/components/source-graphs/source-volume/source-volume.component.spec.ts
+++ src/app/subject/components/source-graphs/source-volume/source-volume.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
 import { StoreModule, combineReducers } from '@ngrx/store'
 
+import { ChartBaseBarComponent } from '../../../../charts/chart-base-bar/chart-base-bar.component'
+import {
+  MockAPISampleDataset,
+  MockTimeFrameChartData,
+  MockTimeIntervalChartData
+} from '../../../../shared/testing/mocks/mock-chart-data'
+import { parseTimeHoles } from '../../../../shared/utils/parse-time-holes'
 import * as fromRoot from '../../../../store'
 import * as fromFeature from '../../../store'
 import { SourceGraphsModule } from '../source-graphs.module'
@@ -9,26 +17,56 @@ import { SourceVolumeComponent } from './source-volume.component'
 describe('SourceVolumeComponent', () => {
   let component: SourceVolumeComponent
   let fixture: ComponentFixture<SourceVolumeComponent>
+  let element: HTMLElement
+
+  const mockChartData = parseTimeHoles(
+    MockAPISampleDataset,
+    MockTimeFrameChartData,
+    MockTimeIntervalChartData
+  )
+  const mockDates = [
+    new Date(MockTimeFrameChartData.startDateTime),
+    new Date(MockTimeFrameChartData.endDateTime)
+  ]
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        SourceGraphsModule,
+        // SourceGraphsModule,
         StoreModule.forRoot({
           ...fromRoot.reducers,
           subject: combineReducers(fromFeature.reducers)
         })
-      ]
+      ],
+      declarations: [ChartBaseBarComponent, SourceVolumeComponent]
     }).compileComponents()
   }))
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SourceVolumeComponent)
     component = fixture.componentInstance
-    fixture.detectChanges()
+    element = fixture.nativeElement
   })
 
   it('should be created', () => {
+    fixture.detectChanges()
+
     expect(component).toBeTruthy()
+  })
+
+  it('should emit data on brush and bind value to this parent component', () => {
+    component.data = mockChartData
+    fixture.detectChanges()
+
+    const comp: ChartBaseBarComponent = fixture.debugElement.query(
+      By.directive(ChartBaseBarComponent)
+    ).componentInstance
+    comp.hasBrush = true
+    comp.categorical = false
+    comp.data = mockChartData
+    comp.brushMove.emit(mockDates)
+    fixture.detectChanges()
+
+    expect(component.sensorDataTimeFrame).toBe(mockDates)
   })
 })
