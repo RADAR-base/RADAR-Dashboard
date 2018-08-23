f3335e30718c9f1dd01197b9d73817d0323cc967
pauline <mpgxvii@gmail.com>
2018-08-23 16:58:02 +0100

    add tests in graph components (tooltip, eventEmitters)

--- src/app/subject/components/source-graphs/source-graph/source-graph.component.spec.ts
+++ src/app/subject/components/source-graphs/source-graph/source-graph.component.spec.ts
@@ -2,30 +2,50 @@ import { NO_ERRORS_SCHEMA } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { StoreModule } from '@ngrx/store'
 
-import { MockSourceData } from '../../../../shared/testing/mocks/mock-sources'
+import { ChartBaseLineComponent } from '../../../../charts/chart-base-line/chart-base-line.component'
+import {
+  MockSensorDataResult,
+  MockSourceData
+} from '../../../../shared/testing/mocks/mock-sources'
 import { reducers } from '../../../store'
 import { SourceGraphComponent } from './source-graph.component'
 
 describe('SourceGraphComponent', () => {
   let component: SourceGraphComponent
   let fixture: ComponentFixture<SourceGraphComponent>
+  let element: HTMLElement
+
+  const mouseEventObject = { clientX: 800, clientY: 300 }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [StoreModule.forRoot(reducers)],
-      declarations: [SourceGraphComponent],
+      declarations: [SourceGraphComponent, ChartBaseLineComponent],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents()
 
     fixture = TestBed.createComponent(SourceGraphComponent)
     component = fixture.componentInstance
+    element = fixture.nativeElement
 
     component.sourceData = MockSourceData
-
-    fixture.detectChanges()
   })
 
   it('should be created', () => {
+    fixture.detectChanges()
     expect(component).toBeTruthy()
+  })
+
+  it('should emit data on tooltipMouseMove', () => {
+    spyOn(component.tooltipMouseMoveOuter, 'emit')
+    component.isLoaded = true
+    component.sensorData = MockSensorDataResult.data
+    fixture.detectChanges()
+
+    const evt = new MouseEvent('mousemove', mouseEventObject)
+    element.querySelector('.tooltip-mouse-box').dispatchEvent(evt)
+
+    expect(element.querySelector('.tooltip-mouse-box')).toBeTruthy()
+    expect(component.tooltipMouseMoveOuter.emit).toHaveBeenCalled()
   })
 })
--- src/app/subject/components/source-graphs/source-graphs.component.spec.ts
+++ src/app/subject/components/source-graphs/source-graphs.component.spec.ts
@@ -2,13 +2,23 @@ import { NO_ERRORS_SCHEMA } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { StoreModule, combineReducers } from '@ngrx/store'
 
+import { ChartBaseLineComponent } from '../../../charts/chart-base-line/chart-base-line.component'
+import {
+  MockSensorsAll,
+  MockSources
+} from '../../../shared/testing/mocks/mock-sources'
 import * as fromRoot from '../../../store'
 import * as fromFeature from '../../store'
+import { SourceGraphComponent } from './source-graph/source-graph.component'
 import { SourceGraphsComponent } from './source-graphs.component'
 
 describe('SourceGraphsComponent', () => {
   let component: SourceGraphsComponent
   let fixture: ComponentFixture<SourceGraphsComponent>
+  let element: HTMLElement
+
+  const mockSources = MockSources
+  const mouseEventObject = { clientX: 700, clientY: 300 }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,20 +28,75 @@ describe('SourceGraphsComponent', () => {
           subject: combineReducers(fromFeature.reducers)
         })
       ],
-      declarations: [SourceGraphsComponent],
+
+      declarations: [
+        SourceGraphsComponent,
+        SourceGraphComponent,
+        ChartBaseLineComponent
+      ],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents()
 
     fixture = TestBed.createComponent(SourceGraphsComponent)
     component = fixture.componentInstance
+    element = fixture.nativeElement
 
-    component.sensorsData = []
     component.sources = []
-
-    fixture.detectChanges()
+    component.isDataLoaded = false
+    component.sensorsData = []
+    component.volumeData = []
+    component.isVolumeDataLoaded = false
+    component.volumeTimeFrame = {
+      startDateTime: new Date(),
+      endDateTime: new Date()
+    }
+    component.sensorsDataTimeFrame = {
+      startDateTime: new Date(),
+      endDateTime: new Date()
+    }
   })
 
   it('should be created', () => {
+    component.sources = []
+    component.sensorsData = []
+
     expect(component).toBeTruthy()
+  })
+
+  it('should not show tooltip when mouse is not within target location', () => {
+    component.sources = mockSources
+    fixture.detectChanges()
+
+    const evt = new MouseEvent('mousemove', { clientX: 0, clientY: 0 })
+    element.querySelector('.source').dispatchEvent(evt)
+
+    expect(component.tooltipShow).toBe(0)
+  })
+
+  it('should not show tooltip on mouseleave', () => {
+    component.sources = mockSources
+    component.sensorsData = MockSensorsAll
+    component.isDataLoaded = { rJywbfpZm: true }
+
+    fixture.detectChanges()
+
+    component.tooltipShow = 1
+
+    const evt = new MouseEvent('mouseleave', {})
+    element.querySelector('.tooltip-mouse-box').dispatchEvent(evt)
+
+    expect(component.tooltipShow).toBe(0)
+  })
+
+  it('should  show tooltip when mouse is within target', () => {
+    component.sources = mockSources
+    component.sensorsData = MockSensorsAll
+    component.isDataLoaded = { rJywbfpZm: true }
+    fixture.detectChanges()
+
+    const evt = new MouseEvent('mousemove', mouseEventObject)
+    element.querySelector('.tooltip-mouse-box').dispatchEvent(evt)
+
+    expect(component.tooltipShow).toBe(1)
   })
 })
