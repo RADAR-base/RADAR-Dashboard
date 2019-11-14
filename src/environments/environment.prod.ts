5c7e8a9fee3a1b39c290a49f39dc2ef00e150e44
mpgxvii <mpgxvii@gmail.com>
2019-11-14 23:33:33 +0000

    fix env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -16,12 +16,13 @@ export const ENV = {
 
   // AUTH API
   AUTH_URI:
-    'https://radar-cns-platform.rosalind.kcl.ac.uk/managementportal/oauth/token',
+    'https://radar-cns-platform.rosalind.kcl.ac.uk/managementportal/oauth',
   AUTH: {
-    grant_type: 'client_credentials',
+    grant_type: 'authorization_code',
     client_id: 'radar_dashboard',
-    client_secret: 'says_PRETTY_smiled',
+    client_secret: '',
     scope:
-      'SOURCETYPE.READ PROJECT.READ SOURCE.READ SUBJECT.READ MEASUREMENT.READ'
+      'SOURCETYPE.READ PROJECT.READ SOURCE.READ SUBJECT.READ MEASUREMENT.READ',
+    redirect_uri: 'http://localhost:4200/login'
   }
 }
